9b1383915c454c4bdd39c20fa99267a064b2560e
Anar <anar@localhost.localdomain>
2023-11-05 10:50:11 -0800

    added search

--- pwdgui.py
+++ pwdgui.py
@@ -4,13 +4,32 @@ import dbwrapper
 
 
 
-class pwdApp(tk.Frame):
+class pwdApp(tk.Tk):
     def __init__(self):
         super().__init__()
+        
+        self.createWidgets()
     
     def createWidgets(self):
-        mainframe=tk.Frame(self)
+        
+        mainframe=tk.Frame(self,width=200,height=200)
+        unamef=tk.Entry(mainframe,)
+        unamef.pack(side='left')
+        pwdent=tk.Entry(mainframe,)
+        pwdent.pack(side='left')
+        savebutt=tk.Button(mainframe,text="save")
+        savebutt.pack()
+        self.title("password manager")
+        
         mainframe.pack()
+        
+        searchframe=mainframe=tk.Frame(self,width=200,height=200)
+        searchbox=unamef=tk.Entry(searchframe,)
+        searchbox.pack(side='left')
+        searchbutt=tk.Button(searchframe,text="search")
+        searchbutt.pack()
+        savebutt.pack()
+        searchframe.pack(side="left")
 
 
 if __name__ == "__main__":

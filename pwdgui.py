75d90e008de174d7e301dd98306326007c990775
Anar Abbasov <anar_abbasov@hotmail.com>
2023-11-05 11:13:19 -0800

    test

--- pwdgui.py
+++ pwdgui.py
@@ -12,6 +12,9 @@ class pwdApp(tk.Tk):
     
     def createWidgets(self):
         
+        
+        
+        
         mainframe=tk.Frame(self,width=200,height=200)
         unamef=tk.Entry(mainframe,)
         unamef.pack(side='left')
@@ -20,7 +23,10 @@ class pwdApp(tk.Tk):
         savebutt=tk.Button(mainframe,text="save")
         savebutt.pack()
         self.title("password manager")
-        
+        menubar = tk.Menu(mainframe)
+        mainmenu=tk.Menu(menubar,tearoff=0)
+        mainmenu.add_command(label="New",)
+        self.config(menu=menubar)
         mainframe.pack()
         
         searchframe=mainframe=tk.Frame(self,width=200,height=200)
@@ -30,7 +36,7 @@ class pwdApp(tk.Tk):
         searchbutt.pack()
         savebutt.pack()
         searchframe.pack(side="left")
-
+        
 
 if __name__ == "__main__":
     app = pwdApp()
